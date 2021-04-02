6cef84c63bed14aded02f7f77319eca847de6def
dragonrathony <peacehoney425@gmail.com>
2021-04-02 04:55:57 +0100

    update webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/assets/js/app.js', 'public/js/app.js')
+    .sass('resources/assets/sass/app.scss', 'public/css/app.css')
+    .version();
